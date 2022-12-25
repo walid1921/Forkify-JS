4aa57d529a49b47cd2ba92fbdebdd7a57b9056cb
walidka <kouiderayadwalid@gmail.com>
2022-12-25 23:15:17 +0200

    initial commit

---
+++ src/js/config.js
@@ -0,0 +1,6 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'ef814757-2143-4fec-938c-14bd412a1176';
+
+export const MODAL_CLOSE_SEC = 2.5;
